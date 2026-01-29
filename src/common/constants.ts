e9308755a8092cb952ffb07d539fe22e87fe0d6d
tekintian <tekintian@gmail.com>
2026-01-29 22:30:03 +0800

    const

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,12 +1,12 @@
 /**
  * constants
- * @author: oldj
- * @homepage: https://oldj.net
+ * @author: tekintian
+ * @homepage: https://dev.tekin.cn
  */
 
 export const server_url = 'https://dev.tekin.cn'
-export const homepage_url = `${server_url}/home/`
-export const download_url = `${server_url}/download/`
+export const homepage_url = `${server_url}/`
+export const download_url = `https://github.com/tekintian/SwitchHosts/releases`
 export const source_url = 'https://github.com/tekintian/SwitchHosts'
 export const feedback_url = 'https://github.com/tekintian/SwitchHosts/issues'
 export const http_api_port = 50761
